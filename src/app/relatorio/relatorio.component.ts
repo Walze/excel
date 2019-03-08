7fb560b773a7315737cc86a5fa6a7322540edeb4
Walze <catufuzgu@gmail.com>
2019-03-08 10:34:00 -0300

    minor improvs

--- src/app/relatorio/relatorio.component.ts
+++ src/app/relatorio/relatorio.component.ts
@@ -12,27 +12,30 @@ export class RelatorioComponent {
   public card: ICard | undefined;
 
   constructor(
-    _p: ProcessosService
+    processosS: ProcessosService
   ) {
-    _p.data.subscribe(data => {
+    processosS.data.subscribe(data => {
       if (!data.length) { return; }
 
-      this.card = data.reduce((prev, curr) => ({
-        processo: curr.processo,
-        table: curr.table.map((t, i) => {
-          t.linhas = t.linhas.map((l, i2) => {
-            l.contador += prev.table[i].linhas[i2].contador;
-            return l;
-          });
-
-          return t;
-        })
-      }));
-
+      this.card = this._processRelatorio(data);
     });
 
-    _p.all();
+    processosS.all();
   }
 
+
+  private _processRelatorio(data: ICard[]) {
+    return data.reduce((prev, curr) => ({
+      processo: curr.processo,
+      table: curr.table.map((t, i) => {
+        t.linhas = t.linhas.map((l, i2) => {
+          l.contador += prev.table[i].linhas[i2].contador;
+          return l;
+        });
+
+        return t;
+      })
+    }));
+  }
 
 }
